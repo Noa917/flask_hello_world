77906490a2dd2889b3092b6365e0d31c53fa3591
Noa917 <153742208+Noa917@users.noreply.github.com>
2024-02-05 22:52:52 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,6 +35,17 @@ def mongraphique():
 def monhistoiregramme():
    return render_template('histoiregramme.html')
  
- 
+ @app.route("/consultation/")
+def ReadBDD():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients;')
+    data = cursor.fetchall()
+    conn.close()
+    
+    # Rendre le template HTML et transmettre les données
+    return render_template('read_data.html', data=data)
+
+
 if __name__ == "__main__":
   app.run(debug=True)
