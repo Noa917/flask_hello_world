dc81dc3050d623e443b17822416a8159f58db1f7
Noa917 <153742208+Noa917@users.noreply.github.com>
2024-02-05 17:02:03 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import json
 from urllib.request import urlopen
-from flask import json, jsonify
-import plotly.express as px
-
-app = Flask(__name__)
-
+from flask import jsonify
+import sqlite3
+                                                                                                                                       
+app = Flask(__name__)                                                                                                                  
+                                                                                                                                       
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
@@ -21,32 +23,13 @@ def meteo():
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('temp', {}).get('day') - 273.15  # Conversion de Kelvin en °C
+        temp_day_value = list_element.get('temp', {}).get('day') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
 @app.route("/rapport/")
 def mongraphique():
-    response = urlopen('https://api.openweathermap.org/data/2.5/forecast/daily?q=Paris,fr&cnt=16&appid=bd5e378503939ddaee76f12ad7a97608')
-    raw_content = response.read()
-    json_content = json.loads(raw_content.decode('utf-8'))
-
-    dates = []
-    temperatures = []
-
-    for list_element in json_content.get('list', []):
-        dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('temp', {}).get('day') - 273.15  # Conversion de Kelvin en °C
-        dates.append(dt_value)
-        temperatures.append(temp_day_value)
-
-    # Création du graphique à colonnes avec Plotly
-    fig = px.bar(x=dates, y=temperatures, labels={'x': 'Jour', 'y': 'Température (°C)'}, title='Températures à Paris pour les 16 prochains jours')
-
-    # Sauvegarde du graphique en fichier HTML dans le répertoire "templates"
-    fig.write_html("templates/graphique.html")
-
     return render_template("graphique.html")
-
+  
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
